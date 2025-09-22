7959a2eb609158f2cab3e732dbbd1340536b9cb2
Zahra Lari <z-lari@users.noreply.github.comgit config --global user.emailgit config --global user.email z-lari@users.noreply.github.com>
2025-09-22 16:10:15 -0400

    person follower node updated

--- ros_behaviors_fsm/ros_behaviors_fsm/person_follower.py
+++ ros_behaviors_fsm/ros_behaviors_fsm/person_follower.py
@@ -3,6 +3,7 @@ This is a person follower node, which directs the Neato to follow a nearby perso
 """
 
 import math
+import statistics
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
@@ -34,6 +35,10 @@ class PersonFollowerNode(Node):
     def run_loop(self):
         """
         Run the person following behavior.
+
+        Publishes: 
+            Twist representing the desired linear and angular velocities to the /cmd_vel
+            topic.
         """
         # return if self._distances and self._angles haven't been populated yet
         if not self._distances[0]:
@@ -41,33 +46,15 @@ class PersonFollowerNode(Node):
 
         msg = Twist()
 
-        # hardcoding points for now (both on the right of the neato)
-        theta_1 = self._angles[250]
-        theta_2 = self._angles[290]
-        a = self._distances[250]
-        b = self._distances[290]
-
-        theta_c = theta_2 - theta_1
-
-        c = math.sqrt(a**2 + b**2 - 2 * a * b * math.cos(theta_c))
-
-        theta_b = math.asin(b / c * math.sin(theta_c))
-
-        theta_pv = theta_1 - theta_b
-
-        # Proportional control
-        theta_out = self.Kp * theta_pv
-
-        msg.angular.z = theta_out
-        msg.linear.x = 0.2
+        [person_angle, person_distance] = self.find_person()
+        [linear_velocity, angular_velocity] = self.follow_person(person_angle, person_distance, 1)
+        msg.angular.z = angular_velocity
+        msg.linear.x = linear_velocity
 
         self.vel_pub.publish(msg)
 
     def process_scan(self, msg):
         """
-        NOT UPDATED (might not need angles)
-        Clean the Lidar scan data and calculate angles.
-
         This function is called whenever a new lidar scan is published on the /scan
         topic. It creates a distances and angles array, which contain the distance
         from the Neato and angle (starting from the front of the Neato and going
@@ -80,19 +67,19 @@ class PersonFollowerNode(Node):
         Returns:
             distances: An array of floats containing the distance in meters of each
                        point in the scan.
-            angles: An array of floats containing the angle (in radians, going
+            angles: An array of floats containing the angle (in degrees, going
                     counterclockwise from the front of the Neato) of each point in
                     the scan.
         """
         for i, distance in enumerate(msg.ranges):
             if self.LASER_SCAN_RANGE_MIN < distance < self.LASER_SCAN_RANGE_MAX:
                 self._distances[i] = distance
-                self._angles[i] = self.LASER_SCAN_ANGLE_INCREMENT * i - math.pi
+                self._angles[i] = i - 180
             else:
                 self._distances[i] = None
                 self._angles[i] = None
 
-    def find_person(distance_array):
+    def find_person(self):
         """
         This function locates where a person is relative to the Neato, assuming they
         are the only large object within a radius of 2 meters.
@@ -105,21 +92,38 @@ class PersonFollowerNode(Node):
             person_angle: a float representing the person's angle in degrees
             person_distance: a float representing the person's distance in degrees
         """
-        # Count the number of valid points (points < 2m away) in distance_array
-        distance_array = [0 for d in distance_array if d > 2]
-        num_points = len(distance_array) - distance_array.count(0)
+        # Count the number of valid points (points <= 2m away) in distance_array
+        distance_array = [0 for d in self._distances if d > 2]
+        point_distances = [d for d in distance_array if d != 0]
+        point_angles = [d.index() for d in distance_array if d != 0] # Test to make sure index() works as expected
+        # Actually, use the angles output by process_scan---just make sure they are in degrees
 
-        # Locate the median value
-        num_nonzero_vals = 0
-        for i in range(distance_array):
-            if i != 0:
-                num_nonzero_vals += 1
-                if num_nonzero_vals == num_points // 2:  # fix this
-                    person_angle = i
-                    person_distance = i  # fix this
-        return person_angle
-        return person_distance
+        person_angle = statistics.median(point_angles)
+        person_distance = statistics.median(point_distances)
 
+        return person_angle, person_distance
+
+    def follow_person(self, person_angle, person_distance, target_distance):
+        """
+        This function enables the Neato to maintain a fixed distance behind a person by taking a person's location
+        as input and outputting the correct linear and angular velocities.
+
+        Args:
+            person_angle: a float representing the person's angle in degrees
+            person_distance: a float representing the person's distance in degrees
+            target_distance: a float representing the ideal distance between the person and Neato
+
+        Returns:
+            linear_velocity: a float representing the linear velocity to which the Neato's wheels should be set
+            angular_velocity: a float representing an angular velocity to which the Neato's wheels should be set
+        """
+
+        if person_angle > 180:
+            person_angle = 0 - (360 - person_angle)
+        linear_velocity = math.max(0, person_distance-target_distance*self.Kp)
+        angular_velocity = person_angle*self.Kp
+
+        return linear_velocity, angular_velocity
 
 def main(args=None):
     rclpy.init(args=args)
